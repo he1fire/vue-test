393a844cdbdea8aa2b7ce410d210ac5c6b0a3d04
he1fire <cmg3172@naver.com>
2025-06-23 16:43:21 +0900

    test boardgamegeek API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { ref, onMounted } from "vue"
+import type { BoardGame } from './types/types.d';
+
+const games = ref<Array<BoardGame>>([]);
+const isLoading = ref(false);
+
+const fetchGames = () => {
+  isLoading.value = true;
+  const url = 'https://boardgamegeek.com/xmlapi2/collection?username=he1fire&stats=1';
+  axios.get(url)
+    .then(response => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(response.data, 'text/xml');
+      const items = xmlDoc.getElementsByTagName('item');
+      games.value = Array.from(items).map(item => {
+        return {
+          objectid: Number(item.getAttribute('objectid')),
+          name: String(item.getElementsByTagName('name')[0].textContent),
+          //thumbnail: ''
+          thumbnail: String(item.getElementsByTagName('thumbnail').length ? item.getElementsByTagName('thumbnail')[0].textContent : ''),
+        };
+      });
+      isLoading.value = false;
+    })
+    .catch(error => {
+      console.error('API 요청 실패:', error);
+    });
+}
+//onMounted(fetchGames);
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>보드게임 목록</h2>
+    <div v-if="isLoading">
+      <p>로딩 중입니다...</p>
+      <!-- 로딩바 컴포넌트 사용 가능 -->
+    </div>
+    <ul v-else>
+      <li v-for="game in games" :key="game.objectid">
+        <img v-if="game.thumbnail" :src="game.thumbnail" :alt="game.name" style="max-width: 100px; max-height: 100px;">
+        <span>{{ game.name }}</span>
+      </li>
+    </ul>
+    <button @click="fetchGames">목록 불러오기</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/types/types.d.ts
@@ -0,0 +1,5 @@
+export interface BoardGame { // 플레이어
+  objectid: number,
+  name: string,
+  thumbnail: string,
+}
